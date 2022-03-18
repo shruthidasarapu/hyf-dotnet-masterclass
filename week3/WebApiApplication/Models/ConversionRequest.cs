7b297f13a9d853ce6caa4b1e66c38d23234fd347
shruthi <shruthi.dasarapu@gmail.com>
2022-03-18 10:24:15 +0100

    Added week3 homework

---
+++ week3/WebApiApplication/Models/ConversionRequest.cs
@@ -0,0 +1,17 @@
+namespace WebApiApplication.Models;
+public class ConversionRequest
+{
+    public double Value { get; set; }
+    public ValueType TypeToConvert { get; set; }
+    public enum ValueType
+    {
+        Miles,
+        Kilometeres,
+        Gallons,
+        Liters,
+        Pounds,
+        Kilograms,
+        Yards,
+        Meters
+    }
+}
---
+++ week3/WebApiApplication/Models/Distance.cs
@@ -0,0 +1,5 @@
+public class Distance
+{
+    public int Miles { get; set; }
+    public double Kilometers { get; set; }
+}
