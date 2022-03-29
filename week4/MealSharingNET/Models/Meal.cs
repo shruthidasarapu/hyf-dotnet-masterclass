283c6642352faadd16c41fdf6eeaf4a92388a291
shruthi <shruthi.dasarapu@gmail.com>
2022-03-29 13:49:51 +0200

    changed to decimal

--- week4/MealSharingNET/Models/Meal.cs
+++ week4/MealSharingNET/Models/Meal.cs
@@ -4,7 +4,7 @@ public class Meal
     public int ID { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public int Cost { get; set; }
+    public decimal Cost { get; set; }
     public string ImageUrl { get; set; }
     public string Location { get; set; }
     public int MaxReservations { get; set; }
