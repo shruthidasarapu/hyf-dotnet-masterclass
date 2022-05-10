f07a4385daba877615315d38667c503baad312fe
shruthi <shruthi.dasarapu@gmail.com>
2022-05-10 19:19:18 +0200

    modify shared folder

--- week5/MealSharingApp/Services/Shared.cs
+++ week5/MealSharingApp/Services/Shared.cs
@@ -1,4 +1,6 @@
 public class Shared
 {
-    public const string ConnectionString = "Server=localhost;Database=mealapplication;Uid=root;Pwd=password;";
+    public static string ConnectionString = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MYSQLCONNSTR_mealSharingdb")) ?
+   "Server=localhost;Database=mealapplication;Uid=root;Pwd=password;Convert Zero Datetime=True" :
+   Environment.GetEnvironmentVariable("MYSQLCONNSTR_mealSharingdb");
 }
